164da639d5eea1a8fd90264be6964b5222dd2179
wessel-henkes <wessel.henkes@casema.nl>
2017-06-17 13:25:54 +0200

    Ready to deploy

--- src/main/webapp/login.js
+++ src/main/webapp/login.js
@@ -5,13 +5,17 @@ $('.message a').click(function(){
 $("#post").click(function(event){
 	var data = $("#login").serialize();
 	console.log("data = "+data);
-	
+	$.post("restservices/app/team",data,function(response){
+		console.log(data)
+	})
+//		window.sessionStorage.setItem("sessionToken", response);
+		
 	$.post("restservices/authentication",data,function(response){
 		window.sessionStorage.setItem("sessionToken", response);
 
 		sessionStorage.setItem('sessionToken',response);
 		console.log("sessionToken = "+response);
-		window.location.href = 'les8_practicum1.html';
+//		window.location.href = 'les8_practicum1.html';
 	}).fail(function(jqXHR, textStatus, errorThrown) {
 		console.log(textStatus);
 		console.log(errorThrown);
@@ -22,3 +26,27 @@ $("#post").click(function(event){
 $('#guest').click(function(){
 	window.location.href = 'les8_practicum1.html';
 });
+
+
+function test(){
+	$.ajax({
+		url: "restservices/countries/team/username=qwerty&password=admin",
+		method: "POST",
+		beforeSend: function (xhr) {
+		var token = window.sessionStorage.getItem("sessionToken");
+		xhr.setRequestHeader( 'Authorization', 'Bearer ' + token);
+		},
+		success: function (data) {console.log(data);}
+
+		})}
+
+function test2(){
+	 $.post("restservices/countries/team",
+			    {
+			        username: "paul",
+			        password: "qwerty"
+			    },
+			    function(data, status){
+			        alert("Data: " + data + "\nStatus: " + status);
+			    });
+}
