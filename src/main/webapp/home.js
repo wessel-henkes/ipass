acbde1808bd702b97ae45d46a926a4d388a8113e
wessel-henkes <wessel.henkes@casema.nl>
2017-06-19 22:13:14 +0200

    update html

--- src/main/webapp/home.js
+++ src/main/webapp/home.js
@@ -6,17 +6,23 @@ function loadWedstrijden(){
 		console.log(response)
 		})	
 	}	
-	
-//	$.ajax({
-//		url: "restservices/app/wedstrijden",
-//		method: "POST",
-//		data:{ team_id: window.sessionStorage.getItem("team_id")},
-//		beforeSend: function (xhr) {
-//		var token = window.sessionStorage.getItem("sessionToken");
-//		xhr.setRequestHeader( 'Authorization', 'Bearer ' + token);
-//		},
-//		
-//		success: function (data) {
+
+//$.ajaxSetup({
+//	   beforeSend: myFunc,
+//	   complete: myCompleteFunc
+//	});
+	$.ajax({
+		url: "restservices/app/wedstrijden",
+		method: 'POST',
+		data:"{ team_id: "+window.sessionStorage.getItem("team_id")+"}",
+		beforeSend: function (xhr) {
+		var token = window.sessionStorage.getItem("sessionToken");
+		xhr.setRequestHeader( 'Authorization', 'Bearer ' + token);
+		},
+		
+		success: function (data) {
+			console.log(data)
+		}
 //		/* Handle countryList */
 //			$.each(data,function(i, item){
 //				console.log(item);
@@ -27,7 +33,7 @@ function loadWedstrijden(){
 ////				var deleteCountry = "onclick=' deleteCountry("+test+item.code+test+")'";
 ////				var wijzigCountry = "onclick='updateCountry("+test+item.code+test+","+test+item.name+test+","+test+item.continent+test+","+test+item.region+test+","+test+item.surfacearea+test+","+test+item.indepyear+test+","+test+item.population+test+","+test+item.lifeexpectancy+test+","+test+item.gnp+test+","+test+item.gnpold+test+","+test+item.localname+test+","+test+item.governmentform+test+","+test+item.headofstate+test+","+test+item.code2+test+","+test+item.latitude+test+","+test+item.longitude+test+","+test+item.capital+test+")'";
 ////				$("table").append("<tr><td class="+test+"code"+test+">" + item.code +"</td><td class="+test+"name"+test+">" + item.name + "</td><td class="+test+"continent"+test+">" + item.continent + "</td><td class="+test+"region"+test+">" + item.region + "</td><td class="+test+"surf"+test+">" + item.surfacearea + "</td><td class="+test+"ind"+test+x+">" + item.indepyear + "</td><td class="+test+"pop"+test+">" + item.population + "</td><td class="+test+"life"+test+x+">" + item.lifeexpectancy + "</td><td class="+test+"gnp"+test+x+">" + item.gnp + "</td><td class="+test+"gnpold"+test+x+">" + item.gnpold + "</td><td class="+test+"localname"+test+x+">" + item.localname + "</td><td class="+test+"gov"+test+x+">" + item.governmentform + "</td><td class="+test+"head"+test+x+">" + item.headofstate + "</td><td class="+test+"code2"+test+">" + item.code2 + "</td><td class="+test+"latitude"+test+x+">" + item.latitude + "</td><td class="+test+"longitude"+test+x+">" + item.longitude + "</td><td class="+test+"capital"+test+">" + item.capital + "</td><td class="+test+"wijzig"+test+" "+wijzigCountry+"><b>Wijzig</b> </td><td class="+test+"delete"+test+" "+deleteCountry+"><b>Delete</b> </td></tr>")
-//			})
-//		}})
+			})
+
 ////		table();
 //		console.log("countries loaded");
