eb82a08db1b7a7a8d2e115157a983ce0b029d1fa
wessel-henkes <wessel.henkes@casema.nl>
2017-06-20 19:55:35 +0200

    update html

--- src/main/webapp/aanwezig.js
+++ src/main/webapp/aanwezig.js
@@ -15,9 +15,9 @@ $.ajax({
 			
 			$.each(data,function(i, item){
 				console.log(item);
-				$("#Spelers").append('<input type="checkbox" name="aanwezig" value="'+item.id+'"/>'+item.naam+'<br>')
+				$("#Spelers").append('<input type="checkbox" name="speler_id" value="'+item.id+'"/>'+item.naam+'<br>')
 		})
-			$("#Spelers").append('<input type="submit">')
+			$("#Spelers").append('<button id="submit">opslaan</button>')
 				
 		}
 	})
@@ -37,13 +37,41 @@ function load(){
 }
 
 
-$('#Spelers').validate({
-    rules: {
-        checkbox_name: {
-           required: true,
-           minlength: 9  // at least two checkboxes are required
-           // maxlength: 4 // less than 5 checkboxes are required
-           // rangelength: [2,4] // must select 2, 3, or 4 checkboxes
-        }
+$("#submit").click(function(e) {
+    if ($(":checked").length >= 9){
+        // Submit your form here
+    	console.log($("#spelers").serialize())
+    	
+	}else {
+        // Display warning here
+    	alert("Er moeten minimaal 9 spelers aanwezig zijn.");
     }
 });
+
+
+
+function SetSpelers(team_id){
+	$.ajax({
+			url: "restservices/app/spelers",
+			method: 'POST',
+			data:{"team_id":team_id},
+			beforeSend: function (xhr) {
+			var token = window.sessionStorage.getItem("sessionToken");
+			xhr.setRequestHeader( 'Authorization', 'Bearer ' + token);
+			},
+			
+			success: function (data) {
+				console.log(data);
+				/* Handle data */
+				
+				$.each(data,function(i, item){
+					console.log(item);
+					$("#Spelers").append('<input type="checkbox" name="aanwezig" value="'+item.id+'"/>'+item.naam+'<br>')
+			})
+				$("#Spelers").append('<input type="submit">')
+					
+			}
+		})
+	}	
+
+
