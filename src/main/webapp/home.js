1b012caf08249835bdb901219c11de2ebad27ccc
wessel-henkes <wessel.henkes@casema.nl>
2017-06-20 18:33:09 +0200

    update html

--- src/main/webapp/home.js
+++ src/main/webapp/home.js
@@ -20,7 +20,7 @@ function loadWedstrijden(){
 				console.log(item);
 				window.sessionStorage.setItem("WedstrijdData"+i, JSON.stringify(item));
 				var x = "'"
-				$("#wedstrijden").append('<div class="col-3 col-m-3 "><div class="card"><div class="container"><h4><b>'+item.team_thuis+' tegen '+item.team_uit+' </b></h4><p>datum: '+item.datum+'</p><p>tijd:'+item.tijd+'</p><button class="button"><span><b>opstelling bekijken</b></span></button><button class="button" onclick='+x+'opstellingInvoeren('+x+i+x+')'+x+'><span><b>opstelling invoeren</b></span></button><button class="button" onclick=""><span><b>opstelling aanpassen</b></span></button></div></div></div>');
+				$("#wedstrijden").append('<div class="col-3 col-m-3 "><div class="card"><div class="container"><h4><b>'+item.team_thuis+' tegen '+item.team_uit+' </b></h4><p>datum: '+item.datum+'</p><p>tijd:'+item.tijd+'</p><button class="button"><span><b>opstelling bekijken</b></span></button><button class="button" onclick='+x+'opstellingInvoeren('+i+')'+x+'><span><b>opstelling invoeren</b></span></button><button class="button" onclick=""><span><b>opstelling aanpassen</b></span></button><button class="button"><span><b>opstelling verwijderen</b></span></button></div></div></div>');
 			})
 			} else {
 				$.each(data,function(i, item){
