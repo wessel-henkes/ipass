e555ace62be2d8b0f8c7ee5ec7e0200c12f15958
wessel-henkes <wessel.henkes@casema.nl>
2017-06-22 10:09:07 +0200

    html update

--- src/main/webapp/home.js
+++ src/main/webapp/home.js
@@ -20,7 +20,7 @@ function loadWedstrijden(){
 				console.log(item);
 				window.sessionStorage.setItem("WedstrijdData"+i, JSON.stringify(item));
 				var x = "'"
-				$("#wedstrijden").append('<div class="col-3 col-m-3 "><div class="card"><div class="container"><h4><b>'+item.team_thuis+' tegen '+item.team_uit+' </b></h4><p>datum: '+item.datum+'</p><p>tijd:'+item.tijd+'</p><button class="button"><span><b>opstelling bekijken</b></span></button><button id="invoeren'+i+'" class="button" onclick='+x+'opstellingInvoeren('+i+')'+x+'><span><b>opstelling invoeren</b></span></button><button class="button" onclick='+x+'opstellingInvoeren('+i+')'+x+'><span><b>opstelling aanpassen</b></span></button><button class="button" onclick='+x+'opstellingVerwijderen('+item.id+')'+x+'><span><b>opstelling verwijderen</b></span></button></div></div></div>');
+				$("#wedstrijden").append('<div class="col-3 col-m-3 "><div class="card"><div class="container"><h4><b>'+item.team_thuis+' tegen '+item.team_uit+' </b></h4><p>datum: '+item.datum+'</p><p>tijd:'+item.tijd+'</p><button class="button" onclick='+x+'opstellingBekijken('+i+')'+x+'><span><b>opstelling bekijken</b></span></button><button id="invoeren'+i+'" class="button" onclick='+x+'opstellingInvoeren('+i+')'+x+'><span><b>opstelling invoeren</b></span></button><button class="button" onclick='+x+'opstellingInvoeren('+i+')'+x+'><span><b>opstelling aanpassen</b></span></button><button class="button" onclick='+x+'opstellingVerwijderen('+item.id+')'+x+'><span><b>opstelling verwijderen</b></span></button></div></div></div>');
 			})
 			} else {
 				$.each(data,function(i, item){
@@ -37,9 +37,14 @@ function loadWedstrijden(){
 function opstellingInvoeren(i){
 	window.sessionStorage.setItem("WedstrijdData", window.sessionStorage.getItem("WedstrijdData"+i));
 	window.sessionStorage.setItem("WedstrijdStatus", "aanwezig_thuis");
-	window.location.href = 'aanwezig.html';
+	window.location.href = 'opstelling.html';
 }
 
+function opstellingBekijken(i){
+	window.sessionStorage.setItem("WedstrijdData", window.sessionStorage.getItem("WedstrijdData"+i));
+	window.sessionStorage.setItem("WedstrijdStatus", "aanwezig_thuis");
+	window.location.href = 'aanwezig.html';
+}
 
 function opstellingVerwijderen(wedstrijd_id){
 	var uri = "restservices/app/opstelling";
---
+++ src/main/webapp/opstelling.js
@@ -0,0 +1,95 @@
+window.onload = load();
+function getSpelers(team_id){
+$.ajax({
+		url: "restservices/app/spelers",
+		method: 'POST',
+		data:{"team_id":team_id},
+		beforeSend: function (xhr) {
+		var token = window.sessionStorage.getItem("sessionToken");
+		xhr.setRequestHeader( 'Authorization', 'Bearer ' + token);
+		},
+		
+		success: function (data) {
+			console.log(data);
+			/* Handle data */
+			
+			$.each(data,function(i, item){
+				console.log(item);
+				$("#Spelers").append('<input id="Speler_Checkbox'+i+'" class="Speler_Checkbox" type="checkbox" value="'+item.id+'"/>'+item.naam+'<br>')
+			})
+		}
+	})
+}	
+function load(){
+	var item = JSON.parse(window.sessionStorage.getItem("WedstrijdData"));
+	console.log(item);
+	var deel = window.sessionStorage.getItem("WedstrijdStatus");
+	console.log(deel);
+	if (deel == "aanwezig_thuis"){
+		$(".header").append('<h1>Aanwezigheid '+item.team_thuis+'</h1>');
+		getSpelers(item.team_thuis_id);
+	} else if (deel == "aanwezig_uit"){
+		$(".header").append('<h1>Aanwezigheid '+item.team_uit+'</h1>');
+		getSpelers(item.team_uit_id);
+	}
+}
+
+
+
+$('#save_value').click(function () {
+    var arr = $('.Speler_Checkbox:checked').map(function () {
+        return this.value;
+    }).get();
+    console.log(arr);
+    if (arr.length>=9){
+	console.log("submit");
+	SetSpelers(arr);
+    }else {
+    	// Display warning here
+    	alert("Er moeten minimaal 9 spelers aanwezig zijn.");
+    }
+});
+
+function SetSpelers(speler_id_arr){
+	var item = JSON.parse(window.sessionStorage.getItem("WedstrijdData"));
+	var deel = window.sessionStorage.getItem("WedstrijdStatus");
+	var wedstrijd_id =item.id;
+	if (deel == "aanwezig_thuis"){
+		var team_id = item.team_thuis_id;
+		$.ajax({
+				url: "restservices/app/opstelling/aanwezig",
+				method: 'PUT',
+				data:{"team_id":team_id,"wedstrijd_id":wedstrijd_id,"speler_id_arr":speler_id_arr},
+				beforeSend: function (xhr) {
+					var token = window.sessionStorage.getItem("sessionToken");
+					xhr.setRequestHeader( 'Authorization', 'Bearer ' + token);
+				},
+				
+				success: function (data) {
+					console.log(data);
+					window.sessionStorage.setItem("WedstrijdStatus","aanwezig_uit")
+					window.location.href = 'aanwezig.html';
+				}
+		})
+		
+	}else if (deel == "aanwezig_uit"){
+		var team_id = item.team_uit_id;
+		$.ajax({
+			url: "restservices/app/opstelling/aanwezig",
+			method: 'PUT',
+			data:{"team_id":team_id,"wedstrijd_id":wedstrijd_id,"speler_id_arr":speler_id_arr},
+			beforeSend: function (xhr) {
+				var token = window.sessionStorage.getItem("sessionToken");
+				xhr.setRequestHeader( 'Authorization', 'Bearer ' + token);
+			},
+			
+			success: function (data) {
+				console.log(data);
+				window.sessionStorage.setItem("WedstrijdStatus","slagvolgorde_thuis")
+				window.location.href = 'slagvolgorde.html';
+			}
+			
+	})
+	}
+	
+}
