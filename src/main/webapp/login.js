9cc769bda7f30b14565b09c2d20c962f174470bd
wessel-henkes <wessel.henkes@casema.nl>
2017-06-28 13:55:52 +0200

    opstelling fix

--- src/main/webapp/login.js
+++ src/main/webapp/login.js
@@ -2,15 +2,7 @@ $('.message a').click(function(){
    $('form').animate({height: "toggle", opacity: "toggle"}, "slow");
 });
 
-$("#post").click(function(event){
-	 var username = document.getElementById("#username");
-	 var password= document.getElementById("#password");
-	 if(username.validity.checkValidity()){
-		 //test
-	 
-		 break;
-	 }
-	 
+function submitform(){
 	var data = $("#login").serialize();
 	console.log("data = "+data);
 	$.post("restservices/app/team",data,function(response){
@@ -34,7 +26,8 @@ $("#post").click(function(event){
 		alert("De opgegeven gebruikersnaam of het wachtwoord is onjuist.")
 		});
 
-})
+}
+//)
 
 $('#guest').click(function(){ //nog weghalen
 	window.location.href = 'les8_practicum1.html';
